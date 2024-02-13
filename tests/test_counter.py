f469f80e78bf0ba38589abd3349ce6002fab2c48
SY <94941799+FPSwitch@users.noreply.github.com>
2024-02-12 23:23:09 -0800

    Update test_counter.py
    switched status

--- tests/test_counter.py
+++ tests/test_counter.py
@@ -39,6 +39,6 @@ class CounterTest(TestCase):
     def test_delete_counter_successful(self):
         counter_name_to_delete = "example_counter"
         delete_result = self.client.post(counter_name_to_delete)
-        self.assertEqual(delete_result.status_code, status.HTTP_201_CREATED)
-        delete_result = delete_counter(counter_name_to_delete)      #delete counter
         self.assertEqual(delete_result.status_code, status.HTTP_404_NOT_FOUND)
+        delete_result = delete_counter(counter_name_to_delete)      #delete counter
+        self.assertEqual(delete_result.status_code, status.HTTP_201_CREATED)
