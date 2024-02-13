e1a638e6ba92ae59b78d5e85b1cbb9d4f239ff8f
SY <94941799+FPSwitch@users.noreply.github.com>
2024-02-12 20:00:11 -0800

    Update counter.py
    added delete counter and edited indents

--- src/counter.py
+++ src/counter.py
@@ -13,13 +13,16 @@ COUNTERS = {}
 # on this function is "POST".
 @app.route('/counters/<name>', methods=['POST'])
 def create_counter(name):
-  """Create a counter"""
-  app.logger.info(f"Request to create counter: {name}")
-  global COUNTERS
-  if name in COUNTERS:
-    return {"Message":f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
-  COUNTERS[name] = 0
-  return {name: COUNTERS[name]}, status.HTTP_201_CREATED
+    """Create a counter"""
+    app.logger.info(f"Request to create counter: {name}")
+    global COUNTERS
+    if name in COUNTERS:
+        return {"Message":f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
+    COUNTERS[name] = 0
+    return {name: COUNTERS[name]}, status.HTTP_201_CREATED
 
 def setUp(self):
-  self.client = app.test_client()
+    self.client = app.test_client()
+def delete_counter_successful(self):
+    """Delete a counter"""
+  
